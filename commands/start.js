e8c2d452673ef2f06f9d02f3f1e9d8fbec445583
RunTheBot <58890327+ipoi@users.noreply.github.com>
2021-04-16 12:32:54 -0400

    Update start.js

--- commands/start.js
+++ commands/start.js
@@ -4,15 +4,19 @@ const settings = require('./config.json');
 exports.run = async (client, message, args) => {
 	//code to run when command is sent
 	exec(settings.start, (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
-});
-  
+		if (error) {
+			console.log(`error: ${error.message}`);
+			return;
+		}
+		if (stderr) {
+			console.log(`stderr: ${stderr}`);
+			message.channel.send( " Error! Please message Owner. ")
+			return;
+		}
+		if (stdout) {
+			message.channel.send( "Starting! Just a minute :) ")		
+		}
+		console.log(`stdout: ${stdout}`);
+	});
+
 }
