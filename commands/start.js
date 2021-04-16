a76fd9543f30ff6129bddd02c5c70d8d42abd12d
RunTheBot <58890327+ipoi@users.noreply.github.com>
2021-04-16 12:18:06 -0400

    Update start.js

--- commands/start.js
+++ commands/start.js
@@ -1,1 +1,17 @@
+const { exec } = require("child_process");
 
+exports.run = async (client, message, args) => {
+	//code to run when command is sent
+	exec("ls -la", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
+  
+}
